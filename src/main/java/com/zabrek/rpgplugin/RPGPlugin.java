970c0f43fdbbe3fd4fac34d4b4f455ffb8eeb976
gabriel <gabrieldeolival2@gmail.com>
2026-07-15 16:30:30 -0400

    refactor (database): An interface is added to enable proper “dependency injection”

--- src/main/java/com/zabrek/rpgplugin/RPGPlugin.java
+++ src/main/java/com/zabrek/rpgplugin/RPGPlugin.java
@@ -1,18 +1,17 @@
 package com.zabrek.rpgplugin;
 
-import com.zabrek.rpgplugin.commands.JumpCommand;
-import com.zabrek.rpgplugin.commands.SkillsCommand;
+import com.zabrek.rpgplugin.commands.*;
+import com.zabrek.rpgplugin.listeners.*;
 import com.zabrek.rpgplugin.database.DataManager;
+import com.zabrek.rpgplugin.database.types.YMLDataManager;
 
-import com.zabrek.rpgplugin.listeners.OnEntityDamage;
-import com.zabrek.rpgplugin.listeners.OnMenuClick;
-import com.zabrek.rpgplugin.listeners.OnPlayerJoin;
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RPGPlugin extends JavaPlugin {
-    private final DataManager dataManager = new DataManager(this);
+    private final DataManager dataManager = new YMLDataManager(this);
 
     @Override
     public void onEnable() {
@@ -21,19 +20,22 @@ public class RPGPlugin extends JavaPlugin {
         pm.registerEvents(new OnPlayerJoin(dataManager), this);
         pm.registerEvents(new OnEntityDamage(dataManager), this);
         pm.registerEvents(new OnMenuClick(dataManager), this);
+        pm.registerEvents(new OnPlayerQuit(dataManager), this);
 
         // Commands
-        if (this.getCommand("skills") != null) {
-            this.getCommand("skills").setExecutor(new SkillsCommand());
+        PluginCommand SkillsCmd = this.getCommand("skills");
+        if (SkillsCmd != null) {
+            SkillsCmd.setExecutor(new SkillsCommand());
         }
 
-        if (this.getCommand("jump") != null) {
-            this.getCommand("jump").setExecutor(new JumpCommand(dataManager));
+        PluginCommand JumpCmd = this.getCommand("jump");
+        if (JumpCmd != null) {
+            JumpCmd.setExecutor(new JumpCommand(dataManager));
         }
     }
 
     @Override
     public void onDisable() {
-        dataManager.saveAll();
+        dataManager.saveAll(true);
     }
 }
