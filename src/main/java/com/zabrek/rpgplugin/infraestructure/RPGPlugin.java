83e5b23953b8c846cab2f691acba3b3fe6ad3ea0
gabriel <gabrieldeolival2@gmail.com>
2026-07-20 13:24:18 -0400

    refactor(plugin): change the command load and listeners in RPGPlugin

--- src/main/java/com/zabrek/rpgplugin/infraestructure/RPGPlugin.java
+++ src/main/java/com/zabrek/rpgplugin/infraestructure/RPGPlugin.java
@@ -1,6 +1,5 @@
 package com.zabrek.rpgplugin.infraestructure;
 
-import com.zabrek.rpgplugin.domain.shared.DomainRules;
 import com.zabrek.rpgplugin.infraestructure.minecraft.commands.SkillsCommand;
 import com.zabrek.rpgplugin.infraestructure.minecraft.commands.UseCommand;
 import com.zabrek.rpgplugin.infraestructure.minecraft.controllers.ManaController;
@@ -8,13 +7,13 @@ import com.zabrek.rpgplugin.application.ports.out.PlayerRepository;
 import com.zabrek.rpgplugin.infraestructure.database.SQLiteDataManager;
 import com.zabrek.rpgplugin.infraestructure.minecraft.listeners.*;
 import com.zabrek.rpgplugin.infraestructure.minecraft.skills.SkillRegistry;
+
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.command.PluginCommand;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import java.util.List;
 
 public class RPGPlugin extends JavaPlugin {
     private final PlayerRepository dataManager = new SQLiteDataManager(this);
@@ -24,22 +23,13 @@ public class RPGPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        PluginManager pm = Bukkit.getPluginManager();
         dataManager.setup();
 
         this.manaController = new ManaController(this, dataManager);
         Bukkit.getScheduler().runTaskTimer(this, this.manaController, 0L, 40L);
 
-        pm.registerEvents(new OnPlayerJoin(), this);
-        pm.registerEvents(new OnMenuClick(dataManager), this);
-        pm.registerEvents(new OnPlayerConfiguration(dataManager), this);
-        pm.registerEvents(new OnPlayerQuit(dataManager), this);
-        pm.registerEvents(new OnEntityDamage(dataManager, skillRegistry), this);
-        pm.registerEvents(new OnEntityDeath(dataManager, this), this);
-
-        // Commands
-        Objects.requireNonNull(this.getCommand("skills")).setExecutor(new SkillsCommand(this));
-        Objects.requireNonNull(this.getCommand("use")).setExecutor(new UseCommand(dataManager, skillRegistry));
+        registerListeners();
+        registerCommands();
     }
 
     @Override
@@ -47,5 +37,32 @@ public class RPGPlugin extends JavaPlugin {
         manaController.stop();
         dataManager.saveAll(true);
         dataManager.shutdown();
+    }
+
+    private void registerListeners() {
+        PluginManager pm = Bukkit.getPluginManager();
+
+        List.of(
+                new OnPlayerJoin(),
+                new OnMenuClick(dataManager),
+                new OnPlayerConfiguration(dataManager),
+                new OnPlayerQuit(dataManager),
+                new OnEntityDamage(dataManager, skillRegistry),
+                new OnEntityDeath(dataManager, this)
+        ).forEach(listener -> pm.registerEvents(listener, this));
+    }
+
+    private void registerCommands() {
+        registerCommand("skills", new SkillsCommand(this));
+        registerCommand("use", new UseCommand(dataManager, skillRegistry));
+    }
+
+    private void registerCommand(String name, CommandExecutor executor) {
+        var command = this.getCommand(name);
+        if (command == null) {
+            this.getLogger().severe("Critic Error: The command " + name + " is not register on plugin.yml");
+            return;
+        }
+        command.setExecutor(executor);
     }
 }
